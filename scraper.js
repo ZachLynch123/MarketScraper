875b7c2307bd3d717cdd428bdcfb1109acdb93be
Zach Lynch <zachary.lynch1337@gmail.com>
2021-08-15 14:03:00 -0700

    added cheerio and get to project

---
+++ scraper.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+/* const accountSid = 'AC42474977264072da7257d91e3b3f3ed6'
+const authToken = 'a5b783e591e78abfdec60d64b558ec2b'
+
+const client = require('twilio')(accountSid, authToken)
+client.messages.create({
+    body: "test test 1, 2",
+    from: "+14342531690",
+    to: "+17028573658"
+})
+.then(message => console.log(message.sid)); */
